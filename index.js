1747d69130ff861de7073291a61541c844b317a2
Nikola Nenovski <nicknenovski@gmail.com>
2024-07-07 16:06:41 +0300

    view function implementation, searchCity function implementation

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-const requestData = async (city) => {
+const requestData = async city => {
   const key = '6afd1a2efe01865fcae54da8837f0e44';
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`)
-  return response.json();
-}
-
-const evaluateResponse = async (city) => {
-  const response = await requestData(city);
-
-  // change to try / catch
-  if (response.cod === '404') {
-    return 'Not found';
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+  );
+  if (!response.ok) {
+    throw new Error('City not found...');
   }
-  return response;
-}
+  return response.json();
+};
 
-const weatherInfoView = city => `
+const evaluateResponse = async city => {
+  const response = await requestData(city);
+  console.log(response);
+  return response;
+};
+
+const weatherInfoView = async city => `
   <div>
     <h1>${city.name}</h1>
     <p>${new Date(city.dt * 1000)}</p>
     <p>${city.main.temp.toFixed(1)}°C</p>
-    <p>feels like ${city.main.feels_like.toFixed(1)}°C. ${city.weather[0].description}.</p>
+    <p>feels like ${city.main.feels_like.toFixed(1)}°C.</p>
+    <p>${city.weather[0].description}.</p>
+    <hr>
     <p>humidity: ${city.main.humidity}%</p>
+    <img src="${`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}" />
     <p>wind speed: ${city.wind.speed.toFixed(1)} m/S</p>
     <hr>
-    <p>sunrise: ${new Date(city.sys.sunrise * 1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</p>
-    <p>sunset: ${new Date(city.sys.sunset * 1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</p>
+    <p>sunrise: ${new Date(city.sys.sunrise * 1000).toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    })}</p>
+    <p>sunset: ${new Date(city.sys.sunset * 1000).toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    })}</p>
   </div>
-`
+`;
 
+const errorView = error => `
+  <div>
+    <p>${error}</p>
+  </div>
+`;
 
-const renderWeatherDetails = async (city) => {
+const renderWeatherDetails = async city => {
   const cityData = await evaluateResponse(city);
-  console.log(cityData);
   const container = document.querySelector('.info-container');
-  container.innerHTML = weatherInfoView(cityData);
-}
+  container.innerHTML = await weatherInfoView(cityData);
+};
+
+const renderNotFound = error => {
+  const container = document.querySelector('.info-container');
+  container.innerHTML = errorView(error);
+};
+
+const searchCity = async searchInput => {
+  try {
+    await renderWeatherDetails(searchInput);
+  } catch (error) {
+    renderNotFound(error.message);
+  }
+};
 
 const searchButton = document.querySelector('#search-btn');
 
 searchButton.addEventListener('click', event => {
   let input = document.querySelector('#input-search-city');
-  renderWeatherDetails(input.value)
+  searchCity(input.value);
   input.value = '';
-})
+});
